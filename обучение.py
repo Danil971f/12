177692148576112f66ff5ccfabc2c1e2ab277e2d
rodin <rodindanil92@gmail.com>
2025-02-26 00:29:33 +0900

    add file

--- обучение.py
+++ обучение.py
@@ -1,2 +1,13 @@
 print('Hello,world!')
-print("Тяжело, но вроде бы понял немного")
+#сложение
+print(5+3)
+#умножение
+print(4*6)
+#вычетание
+print(7-2)
+#деление
+print(10/3)
+result = 2+3*4
+print(result)
+result = (2+3)*4
+print(result)
